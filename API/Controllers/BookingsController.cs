b9bef1e39eae274ee2e1c9fa5593d2cb621c857c
wach0001 <146825284+wach0001@users.noreply.github.com>
2024-09-18 22:16:40 +0200

    1

--- API/Controllers/BookingsController.cs
+++ API/Controllers/BookingsController.cs
@@ -84,20 +84,7 @@ namespace API.Controllers
             return Ok(bookings);
         }
 
-        [HttpGet("emails/{GuestEmail}")]
-        public async Task<ActionResult<List<Booking>>> GetBookingsByGuestEmail(string GuestEmail)
-        {
-            var bookings = await _hotelContext.Bookings
-                .Where(b => b.GuestEmail == GuestEmail)
-                .Include(b => b.Room)
-                .ToListAsync();
-            if (bookings == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(bookings);
-        }
+        
 
         [HttpGet("phone/{GuestPhoneNr}")]
         public async Task<ActionResult<Booking>> GetBookingByGuestPhoneNr(string GuestPhoneNr)
